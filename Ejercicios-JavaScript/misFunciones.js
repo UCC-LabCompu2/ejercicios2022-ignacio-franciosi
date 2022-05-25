cfcda27e53fa1840d4a21f9c07320796707ee647
ignacio-franciosi <2104338@ucc.edu.ar>
2022-05-25 12:57:38 -0300

    Se hizo ej PrimerWeb y Conversor de Unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,30 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarUnidades (id,valor){
+if(isNaN(valor)){
+alert("Valor invalido"+id);
+    document.lasUnidades.unid_metro.value = "";
+    document.lasUnidades.unid_pie.value = "";
+    document.lasUnidades.unid_yarda.value = "";
+    document.lasUnidades.unid_pulgada.value = "";
+}else if (id=="metro"){
+    document.lasUnidades.unid_pulgada.value = 39.37*valor;
+    document.lasUnidades.unid_yarda.value = 1.09*valor;
+    document.lasUnidades.unid_pie.value = 3.28*valor;
+}else if (id=="pulgada"){
+    document.lasUnidades.unid_metro.value = 0.0254*valor;
+    document.lasUnidades.unid_pie.value = 0.08*valor;
+    document.lasUnidades.unid_yarda.value = 0.02*valor;
+}else if (id=="yarda"){
+    document.lasUnidades.unid_metro.value = 0.91*valor;
+    document.lasUnidades.unid_pie.value = 3*valor;
+    document.lasUnidades.unid_pulgada.value = 36*valor;
+} else if (id=="pie"){
+    document.lasUnidades.unid_metro.value =0.30*valor;
+    document.lasUnidades.unid_yarda.value = 0.33*valor;
+    document.lasUnidades.unid_pulgada.value = 12*valor;
+}
+
+}
